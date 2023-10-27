b3acafd934d1ef900cd7ed030df6a2c5ad2a06d3
Bogdan-Zinovij <76776361+Bogdan-Zinovij@users.noreply.github.com>
2023-10-27 18:16:39 +0300

    fix 2

--- rate.service.js
+++ rate.service.js
@@ -1,4 +1,4 @@
-import fetch from "node-fetch";
+import axios from "axios";
 
 const BTC_TO_UAH_URL =
     "https://api.binance.com/api/v3/ticker/price?symbol=BTCUAH";
@@ -7,29 +7,41 @@ const BTC_TO_USD_URL =
 
 class RateService {
     async getUsdRate() {
-        const response = await fetch(BTC_TO_USD_URL);
-        const data = await response.json();
+        try {
+            const response = await axios.get(BTC_TO_USD_URL);
+            const data = response.data;
 
-        if (!data.price) {
-            throw new Error("Unexpected response from the 3rd party service");
+            if (!data.price) {
+                throw new Error(
+                    "Unexpected response from the 3rd party service"
+                );
+            }
+            const rate = parseInt(data.price);
+            const message = `Current BTC-rate is ${rate} USD`;
+
+            return message;
+        } catch (error) {
+            throw new Error("Error fetching data: " + error.message);
         }
-        const rate = parseInt(data.price);
-        const message = `Current BTC-rate is ${rate} USD`;
-
-        return message;
     }
 
     async getUahRate() {
-        const response = await fetch(BTC_TO_UAH_URL);
-        const data = await response.json();
+        try {
+            const response = await axios.get(BTC_TO_UAH_URL);
+            const data = response.data;
 
-        if (!data.price) {
-            throw new Error("Unexpected response from the 3rd party service");
+            if (!data.price) {
+                throw new Error(
+                    "Unexpected response from the 3rd party service"
+                );
+            }
+            const rate = parseInt(data.price);
+            const message = `Current BTC-rate is ${rate} UAH`;
+
+            return message;
+        } catch (error) {
+            throw new Error("Error fetching data: " + error.message);
         }
-        const rate = parseInt(data.price);
-        const message = `Current BTC-rate is ${rate} UAH`;
-
-        return message;
     }
 }
 
